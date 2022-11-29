ce8cf20113f6b98f03191e4876d9add530ab47e6
LuckDev <lukedru@yahoo.com>
2022-11-28 20:29:07 -0800

    Initial Commit

---
+++ CalculusCar/CalculusCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculusCarApp.swift
+//  CalculusCar
+//
+//  Created by Luke Drushell on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculusCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
